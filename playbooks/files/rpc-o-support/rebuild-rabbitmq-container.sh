717fd6c23160eb4c8ebb645e39ce388d75f28f40
Bjoern Teipel <bjoern.teipel@rackspace.com>
2017-10-02 09:59:11 -0500

    rebuild-rabbitmq-container.sh: minor fixes

--- playbooks/files/rpc-o-support/rebuild-rabbitmq-container.sh
+++ playbooks/files/rpc-o-support/rebuild-rabbitmq-container.sh
@@ -20,15 +20,15 @@ openstack-ansible lxc-containers-create.yml --limit rabbit_mq_container
 openstack-ansible rabbitmq-install.yml
 
 # Re-add OpenStack rabbitmq users
-grep -q nova_rabbitmq_vhost /etc/ansible/roles/os_nova/defaults/main.yml 2>&1
+grep -q nova_rabbitmq_vhost /etc/ansible/roles/os_nova/defaults/main.yml >/dev/null 2>&1
 if [ "$?" -eq 0 ]; then
   TAGS="keystone-rabbitmq,keystone-rabbitmq-user,keystone-rabbitmq-vhost,glance-rabbitmq,glance-rabbitmq-user,glance-rabbitmq-vhost,cinder-rabbitmq,cinder-rabbitmq-user,cinder-rabbitmq-vhost,nova-rabbitmq,nova-rabbitmq-user,nova-rabbitmq-vhost,neutron-rabbitmq,neutron-rabbitmq-user,neutron-rabbitmq-vhost,heat-rabbitmq,heat-rabbitmq-user,heat-rabbitmq-vhost"
 
   ansible 'ceilometer_collector' --list-hosts 2>/dev/null | grep -q "No hosts matched" || TAGS="$TAGS,ceilometer-rabbitmq,ceilometer-rabbitmq-user,ceilometer-rabbitmq-vhost"
 
-  openstack-ansible setup-openstack.yml --tags $TAGS
-else 
-  openstack-ansible setup-openstack.yml --tags rabbitmq-user
+  openstack-ansible setup-everything.yml --tags $TAGS
+else
+  openstack-ansible setup-everything.yml --tags rabbitmq-user
 fi;
 
 if [ -d "/opt/rpc-openstack" ]; then
@@ -40,4 +40,3 @@ if [ -d "/opt/rpc-openstack" ]; then
     openstack-ansible setup-maas.yml --tags maas-infra-rabbitmq
   fi;
 fi;
-
