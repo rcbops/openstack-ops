9d3853ffbd9b389da2d225b092015da6893c750b
Bjoern Teipel <bjoern.teipel@rackspace.com>
2017-10-02 09:33:23 -0500

    rebuild-rabbitmq-container.sh: Adding users for rabbitmq after a rebuild

--- playbooks/files/rpc-o-support/rebuild-rabbitmq-container.sh
+++ playbooks/files/rpc-o-support/rebuild-rabbitmq-container.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+LANG=en_US.UTF-8
+
 cd /opt/rpc-openstack/openstack-ansible/playbooks
 
 eval $@
@@ -15,3 +18,26 @@ done
 openstack-ansible -e container_group="rabbit_mq_container" lxc-containers-destroy.yml
 openstack-ansible lxc-containers-create.yml --limit rabbit_mq_container
 openstack-ansible rabbitmq-install.yml
+
+# Re-add OpenStack rabbitmq users
+grep -q nova_rabbitmq_vhost /etc/ansible/roles/os_nova/defaults/main.yml 2>&1
+if [ "$?" -eq 0 ]; then
+  TAGS="keystone-rabbitmq,keystone-rabbitmq-user,keystone-rabbitmq-vhost,glance-rabbitmq,glance-rabbitmq-user,glance-rabbitmq-vhost,cinder-rabbitmq,cinder-rabbitmq-user,cinder-rabbitmq-vhost,nova-rabbitmq,nova-rabbitmq-user,nova-rabbitmq-vhost,neutron-rabbitmq,neutron-rabbitmq-user,neutron-rabbitmq-vhost,heat-rabbitmq,heat-rabbitmq-user,heat-rabbitmq-vhost"
+
+  ansible 'ceilometer_collector' --list-hosts 2>/dev/null | grep -q "No hosts matched" || TAGS="$TAGS,ceilometer-rabbitmq,ceilometer-rabbitmq-user,ceilometer-rabbitmq-vhost"
+
+  openstack-ansible setup-openstack.yml --tags $TAGS
+else 
+  openstack-ansible setup-openstack.yml --tags rabbitmq-user
+fi;
+
+if [ -d "/opt/rpc-openstack" ]; then
+  cd /opt/rpc-openstack/rpcd/playbooks
+
+  if [ ! -d "/opt/rpc-maas" ]; then
+    openstack-ansible setup-maas.yml --tags rabbitmq-user
+  else
+    openstack-ansible setup-maas.yml --tags maas-infra-rabbitmq
+  fi;
+fi;
+
