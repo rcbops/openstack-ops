a10d265d13258fcf90ac7c421a881e7f285b6fed
Bjoern Teipel <bjoern.teipel@rackspace.com>
2019-03-08 16:52:02 -0600

    Adding new common-rabbitmq tag for newer newton based installs

--- playbooks/files/rpc-o-support/rebuild-rabbitmq-container.sh
+++ playbooks/files/rpc-o-support/rebuild-rabbitmq-container.sh
@@ -16,19 +16,19 @@ for i in $(seq 1 20); do
 done
 
 openstack-ansible -e container_group="rabbit_mq_container" -e force_containers_destroy="yes" -e force_containers_data_destroy="yes" lxc-containers-destroy.yml 
-openstack-ansible lxc-containers-create.yml --limit rabbit_mq_container
+openstack-ansible lxc-containers-create.yml --limit rabbit_mq_container:os-infra_hosts:infra_hosts
 openstack-ansible rabbitmq-install.yml
 
 # Re-add OpenStack rabbitmq users
 grep -q nova_rabbitmq_vhost /etc/ansible/roles/os_nova/defaults/main.yml >/dev/null 2>&1
 if [ "$?" -eq 0 ]; then
-  TAGS="keystone-rabbitmq,keystone-rabbitmq-user,keystone-rabbitmq-vhost,glance-rabbitmq,glance-rabbitmq-user,glance-rabbitmq-vhost,cinder-rabbitmq,cinder-rabbitmq-user,cinder-rabbitmq-vhost,nova-rabbitmq,nova-rabbitmq-user,nova-rabbitmq-vhost,neutron-rabbitmq,neutron-rabbitmq-user,neutron-rabbitmq-vhost,heat-rabbitmq,heat-rabbitmq-user,heat-rabbitmq-vhost"
+  TAGS="keystone-rabbitmq,keystone-rabbitmq-user,keystone-rabbitmq-vhost,glance-rabbitmq,glance-rabbitmq-user,glance-rabbitmq-vhost,cinder-rabbitmq,cinder-rabbitmq-user,cinder-rabbitmq-vhost,nova-rabbitmq,nova-rabbitmq-user,nova-rabbitmq-vhost,neutron-rabbitmq,neutron-rabbitmq-user,neutron-rabbitmq-vhost,heat-rabbitmq,heat-rabbitmq-user,heat-rabbitmq-vhost,common-rabbitmq"
 
   ansible 'ceilometer_collector' --list-hosts 2>/dev/null | grep -q "No hosts matched" || TAGS="$TAGS,ceilometer-rabbitmq,ceilometer-rabbitmq-user,ceilometer-rabbitmq-vhost"
 
-  openstack-ansible setup-everything.yml --tags $TAGS
+  openstack-ansible setup-openstack.yml --tags $TAGS
 else
-  openstack-ansible setup-everything.yml --tags rabbitmq-user
+  openstack-ansible setup-openstack.yml --tags rabbitmq-user
 fi;
 
 if [ -d /opt/rpc-maas/playbooks ]; then
