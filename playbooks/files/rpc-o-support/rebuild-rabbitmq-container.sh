09b93d2ef2c332e6ab161949907bd7918a95b491
Bjoern Teipel <bjoern.teipel@rackspace.com>
2019-04-30 09:24:33 -0500

    Replace os-infra_hosts group
    Not all clouds have os-infra_hosts defined.
    As the result shared-infra_hosts will be used instead

--- playbooks/files/rpc-o-support/rebuild-rabbitmq-container.sh
+++ playbooks/files/rpc-o-support/rebuild-rabbitmq-container.sh
@@ -15,7 +15,7 @@ for i in $(seq 1 20); do
 done
 
 openstack-ansible -e container_group="rabbit_mq_container" -e force_containers_destroy="yes" -e force_containers_data_destroy="yes" lxc-containers-destroy.yml 
-openstack-ansible lxc-containers-create.yml --limit rabbit_mq_container:os-infra_hosts:infra_hosts
+openstack-ansible lxc-containers-create.yml --limit rabbit_mq_container:shared-infra_hosts:infra_hosts
 openstack-ansible rabbitmq-install.yml
 
 # Re-add OpenStack rabbitmq users
