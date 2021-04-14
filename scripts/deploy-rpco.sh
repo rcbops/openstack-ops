ba37351af944c6c561e21d7fba3dc717859d0092
Bjoern Teipel <bjoern.teipel@rackspace.com>
2021-04-14 17:59:02 -0500

    Update deploy-rpco.sh
    Update Ussuri version to latest 21.2.4 and synchronize with current release for upgrades

--- scripts/deploy-rpco.sh
+++ scripts/deploy-rpco.sh
@@ -24,7 +24,7 @@
 set -e
 
 OSA_PYEXE=/opt/ansible-runtime/bin/python3
-OSA_RELEASE="${OSA_RELEASE:-21.2.3}"
+OSA_RELEASE="${OSA_RELEASE:-21.2.4}"
 OSA_TOKEN_GEN="/opt/openstack-ansible/scripts/pw-token-gen.py"
 OSA_INVENTORY="/opt/openstack-ansible/inventory/dynamic_inventory.py"
 OSA_RUN_PLAY="${OSA_RUN_PLAY:-true}"
