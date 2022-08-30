5aec158efd5ef6daaf537a64274d2aa745e9051b
Bjoern Teipel <bjoern.teipel@rackspace.com>
2022-08-30 13:06:40 -0500

    Update update_hp_firmware.py
    Remove override for DL380 as it shares the same firmware as DL360

--- playbooks/files/update_hp_firmware.py
+++ playbooks/files/update_hp_firmware.py
@@ -125,17 +125,9 @@ firmwares["ProLiant DL360 Gen9"] = {
     }
 }
 
+# DL380 shares largely the same firmwares as the DL360
 firmwares["ProLiant DL380 Gen9"] = copy.deepcopy(
     firmwares["ProLiant DL360 Gen9"])
-
-firmwares["ProLiant DL380 Gen9"]["SYSTEM"] = {
-    "check": "ipmitool fru |grep 'MB BIOS' -A5 |awk -F ': ' '/Product Version/ {print $2}'",
-    "ver": "05/17/2022",
-    "fwpkg": "hp_firmware-system-p89-2.96_2022_05_17-1.1.i386.rpm",
-    "md5": "1399a7387b3c21f46e31ffe5d868577a",
-    "inp": "y\nn\n",
-    "ret": 1
-}
 
 # GEN10 definition
 firmwares["ProLiant DL360 Gen10"] = {
