430bc7fa0b01dcaba7c5b02611f706cbc8ece0f1
Bjoern Teipel <bjoern.teipel@rackspace.com>
2023-02-20 23:33:26 -0600

    Update deploy-rpco.sh
    Bump new releases to wallaby

--- scripts/deploy-rpco.sh
+++ scripts/deploy-rpco.sh
@@ -26,7 +26,7 @@
 set -e
 
 OSA_PYEXE=/opt/ansible-runtime/bin/python3
-OSA_RELEASE="${OSA_RELEASE:-22.4.0}"
+OSA_RELEASE="${OSA_RELEASE:-23.4.4}"
 OSA_TOKEN_GEN="/opt/openstack-ansible/scripts/pw-token-gen.py"
 OSA_INVENTORY="/opt/openstack-ansible/inventory/dynamic_inventory.py"
 OSA_RUN_PLAY="${OSA_RUN_PLAY:-true}"
